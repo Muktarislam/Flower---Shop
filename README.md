8aaa1358d9e8a2913bdfbd298d9ccd8b2344d21a
muktar islam <muktarislam95@gmail.com>
2021-06-25 03:02:50 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+live link: https://flower-shop-4325c.web.app
---
+++ src/components/Home/firebase.Config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzqPK-mGiW4VHfesM8Du1ekWw1GrRWvno",
+    authDomain: "flower-shop-4325c.firebaseapp.com",
+    projectId: "flower-shop-4325c",
+    storageBucket: "flower-shop-4325c.appspot.com",
+    messagingSenderId: "552366171456",
+    appId: "1:552366171456:web:c074c8c0e89a70a89a46a5"
+  };
+  export default firebaseConfig;
+  
